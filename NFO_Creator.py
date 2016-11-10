a95a507e556d6ea96d4c601439ea12c02abcbc82
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-11-10 16:04:13 +0000

    Main application specifies the rootfolderpath

--- NFO_Creator.py
+++ NFO_Creator.py
@@ -3,5 +3,5 @@ from code import main_application as CrystalDefenders
 
 
 print "Application Started"
-CrystalDefenders.runapplication()
+CrystalDefenders.runapplication("A:\\")
 print "Application Ended"
